57297f5b2029abf57ab6241243c4bfc7255a5873
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2023-01-28 10:28:29 +0300

    Add favorites to the user model

--- src/modules/user.js
+++ src/modules/user.js
@@ -4,7 +4,12 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 2},
   surname: { type: String, required: true, minLength: 2},
   email: { type: String, required: true},
-  password: { type: String, required: true, minLength: 3}
+  password: { type: String, required: true, minLength: 3},
+  favorites: [{
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Movie', 
+    autopopulate: { maxDepth: 1 }
+  }]
 })
 
 UserSchema.plugin(import('mongoose-autopopulate'))
