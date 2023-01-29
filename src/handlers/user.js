833b9a03dfa27724915f408ba8d881085cf5612f
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-01-29 19:06:02 +0300

    Delete favorites of user

--- src/handlers/user.js
+++ src/handlers/user.js
@@ -5,14 +5,17 @@ export const createUser = async (req, res, next) => {
   const { name, surname, email, password } = req.body
 
   try{
-    const user = await UserService.createUser({ 
+    const user ={ 
       name, 
       surname, 
       email, 
-      password: await hashPassword(password) 
-    })
+      password: await hashPassword(password)
+    }
 
-    const token =createJWT({ id: user._id, email: user.email })
+    const newUser = await UserService.createUser(user)
+
+    const token = createJWT({ id: newUser._id, email: newUser.email })
+
     res.json({ token })
   } catch(err){
     return res.json({ error: err.message , description: 'Error creating user'})
