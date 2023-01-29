c6bd23fc9a54ee7d5cb37644b3f982832181f6ea
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2023-01-29 09:15:20 +0300

    Add image feature to the movie module

--- src/modules/movie.js
+++ src/modules/movie.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const MovieSchema = new mongoose.Schema({
   name: { type: String, required: true},
   type: { type: String, required: true},
-  director: { type: String, required: true}
+  director: { type: String, required: true},
+  image: { type: String, required: true},
 })
 
 module.exports = mongoose.model('Movie', MovieSchema)
