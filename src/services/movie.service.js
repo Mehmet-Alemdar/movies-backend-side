c923bde8055805a41fd8d046beea6cdf139fed89
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2023-01-29 09:35:32 +0300

    Create user and movie services

---
+++ src/services/movie.service.js
@@ -0,0 +1,8 @@
+import BaseService from "./base-service"
+import Movie from "../models/movie"
+
+class MovieService extends BaseService {
+
+}
+
+module.exports = new MovieService(Movie)
---
+++ src/services/user-service.js
@@ -0,0 +1,8 @@
+import  BaseService  from "./base-service"
+import  User  from "../models/user"
+
+class UserService extends BaseService {
+
+}
+
+module.exports = new UserService(User)
