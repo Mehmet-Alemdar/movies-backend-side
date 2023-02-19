ceaa2ee1948bdc4c2ec657d8af6fbcb33805852d
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-02-19 15:34:57 +0300

    Set the port path

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ dotent.config()
 
 import app from './src/server.js'
 
+const PORT = process.env.PORT || '5000'
+
 app.listen('5000', () => {
-  console.log('hello on http://localhost:5000');
+  console.log(`hello on http://localhost:${PORT}`);
 })
